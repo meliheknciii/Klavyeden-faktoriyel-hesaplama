bac4781e1bd6f885414b87e580bfe3c8e54cb0e4
meliheknciii <melihekinciofficial@gmail.com>
2023-11-02 22:07:39 +0300

    Klavyeden girilen sayının faktoriyeli
    döngüler ile faktöriyel hesabi.
    for dongüsü

---
+++ Proje Faktoriyel hesaplama (Klavyeden)/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	// klavyeden girilen sayinin faktoriyelini hesaplama 
+	/*
+	int i;
+	int faktor,girilensayi;
+	
+	printf("Sayiyi giriniz:");
+	scanf("%d",&girilensayi);
+	
+	for (i=1;i<=girilensayi;i++)
+	{
+		faktor=faktor*i;
+	}
+	printf("Girilen Sayinin Faktoriyeli: %d",faktor);
+	*/
+
+	return 0;
+}
+
+
